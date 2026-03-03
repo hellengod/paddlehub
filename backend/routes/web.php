d1bced0c95ab83b9e4df2451a367e483094ec74f
Hellen Bianchini <hellenbiangod@gmail.com>
2026-03-03 20:34:18 -0300

    feat: add health-check endpoint for API

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/health', function () {
+    return response()->json(
+        [
+            "success" => true,
+            "message" => "API running",
+            "time" => now()
+        ]
+    );
+});
