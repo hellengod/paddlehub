e7626f3b64aba1998b6722625161cef309d2e0cc
Hellen Bianchini <hellenbiangod@gmail.com>
2026-03-05 11:47:21 -0300

    feat(frontend): setup Vue project with router, base layout and initial pages

---
+++ frontend/src/components/layouts/AppLayout.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="header">
+        <h1>PaddleHub</h1>
+        <nav>
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/rivers">Rios</RouterLink>
+        </nav>
+    </div>
+    <div class="body">
+        <slot></slot>
+    </div>
+</template>
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+<style></style>
---
+++ frontend/src/views/RiversView.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Rios</h1>
+        <p>Em construcao</p>
+    </div>
+</template>
