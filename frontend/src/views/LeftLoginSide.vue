84cc76e96a5bc66f44a5f4ab6d35575746546133
Hellen Bianchini <hellenbiangod@gmail.com>
2026-03-19 14:52:16 -0300

    feat: adiciona logo e texto no lado esquerdo da tela de login e alguns ajustes de css

--- frontend/src/views/LeftLoginSide.vue
+++ frontend/src/views/LeftLoginSide.vue
@@ -1,3 +1,40 @@
 <template>
-    <img src="\imagem-login-fundo.png" alt="">
+    <div class="container-left">
+        <div class="content">
+            <img src="/logo-paddlehub-grande.png" alt="" class="logo-grande">
+            <h1>Conecte rios, <br>pessoas e <span class="highlight">remadas</span></h1>
+        </div>
+    </div>
 </template>
+<style scoped>
+.container-left {
+    background-color: rgb(22, 99, 73);
+    height: 100%;
+    background-image: url(/img-fundo-login.png);
+    background-size: cover;
+    background-position: center;
+    position: relative
+}
+
+.logo-grande {
+    width: 400px;
+    height: auto;
+}
+
+.content {
+    display: flex;
+    flex-direction: column;
+    align-items: left;
+    gap: 0px;
+}
+
+h1,
+span {
+    font-weight: bold;
+
+}
+
+.highlight {
+    color: rgb(22, 99, 73);
+}
+</style>
--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,7 +1,26 @@
 <template>
-<LeftLoginSide></LeftLoginSide>
+    <div class="allsides">
+        <div class="left side">
+            <LeftLoginSide></LeftLoginSide>
+        </div>
+        <div class="right side">
+            <RightLoginSide></RightLoginSide>
+        </div>
+    </div>
 </template>
 <script setup lang="ts">
 import LeftLoginSide from './LeftLoginSide.vue';
+import RightLoginSide from './RightLoginSide.vue';
 </script>
-<style scoped></style>
+<style scoped>
+.allsides {
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-direction: row;
+}
+
+.side {
+    flex: 1;
+}
+</style>
