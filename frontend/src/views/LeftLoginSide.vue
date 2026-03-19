5e79feebd7011e090f447f0920ce726c8d2a6a7b
Hellen Bianchini <hellenbiangod@gmail.com>
2026-03-19 18:41:59 -0300

    feat: ajusta logo e texto do lado esquerdo da tela de login

--- frontend/src/views/LeftLoginSide.vue
+++ frontend/src/views/LeftLoginSide.vue
@@ -3,6 +3,7 @@
         <div class="content">
             <img src="/logo-paddlehub-grande.png" alt="" class="logo-grande">
             <h1>Conecte rios, <br>pessoas e <span class="highlight">remadas</span></h1>
+            <h3>Descubra novos rios, registre suas experiências<br> e encontre outros remadores pleo caminho</h3>
         </div>
     </div>
 </template>
@@ -16,25 +17,39 @@
     position: relative
 }
 
+.container-left::before {
+    content: "";
+    position: absolute;
+    inset: 0;
+    background: rgba(0, 0, 0, 0.4);
+}
+
 .logo-grande {
     width: 400px;
     height: auto;
+    margin-bottom: -45px;
 }
 
 .content {
     display: flex;
     flex-direction: column;
-    align-items: left;
-    gap: 0px;
+    position: relative;
+    z-index: 1;
+    left: 60px;
+}
+
+h1 {
+    color: whitesmoke;
 }
 
 h1,
 span {
     font-weight: bold;
+    font-size: 45px;
 
 }
 
 .highlight {
-    color: rgb(22, 99, 73);
+    color: rgb(34, 153, 113);
 }
 </style>
