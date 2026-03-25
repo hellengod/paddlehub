325c5a24cc57cfee4cfb7b021c1c51b473c4b043
Hellen Bianchini <hellenbiangod@gmail.com>
2026-03-25 15:57:59 -0300

    feat: iniciamos o component RightLoginSide para o formulario de login

--- frontend/src/views/RightLoginSide.vue
+++ frontend/src/views/RightLoginSide.vue
@@ -1,3 +1,50 @@
 <template>
-    
+    <div class="container-right">
+        <div class="login-card">
+            <div class="text-container">
+            <h1>
+                Entrar na sua conta
+            </h1>
+            <p>Que bom te ver por aqui!</p>
+            </div>
+        </div>
+    </div>
 </template>
+
+<style scoped>
+.container-right {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #08161c;
+}
+
+.login-card {
+    width: 100%;
+    max-width: 600px;
+    padding: 50px;
+    border-radius: 15px;
+    background: rgba(33, 86, 109, 0.096);
+    backdrop-filter: blur(10px);
+    border: 1px solid rgba(69, 199, 255, 0.096);
+
+}
+
+.text-container{
+    text-align: center;
+    margin-bottom: 30px;
+}
+
+h1{
+    color: aliceblue;
+    font-size: 40px;
+    margin-bottom: 10px;
+    font-weight: 500;
+}
+
+p{
+    font-size: 20px;
+}
+</style>
