2a9a01b4a6114fe0612b30ccd83b66cc288cf4dd
Hellen Bianchini <hellenbiangod@gmail.com>
2026-06-12 10:10:04 -0300

    feat(frontend): iniciar estrutura da sidebar no AppLayout

--- frontend/src/components/layouts/AppLayout.vue
+++ frontend/src/components/layouts/AppLayout.vue
@@ -1,12 +1,47 @@
 <template>
     <div class="layout-shell">
-        <div class="aside">
-            <p>ola</p>
+            <aside class="sidebar">
+                <div class="sidebar-header">
+                    <img src="\public\logo-paddlehub-grande.png"alt="Logo" class="logo">
+                </div>
+
+                <nav class="sidebar-nav">
+                    <ul>
+                        <li>
+                            <a>
+                                Home
+                            </a>
+                        </li>
+
+                        <li>
+                            <a>
+                                Mapa
+                            </a>
+                        </li>
+
+                        <li>
+                            <a>
+                                Eventos
+                            </a>
+                        </li>
+
+                        <li>
+                            <a>
+                                Configuracao
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+
+                <div class="sidebar-footer">
+                    <a>Perfil</a>
+                    <a>Sair</a>
+                </div>
+            </aside>
         </div>
         <div class="main">
             <slot></slot>
         </div>
-    </div>
 </template>
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
@@ -14,16 +49,28 @@ import { RouterLink } from 'vue-router'
 <style scoped>
 .layout-shell {
     display: flex;
-    min-height: 100vh
+    min-height: 100vh;
+    background: rgba(33, 86, 109, 0.096);
+
 }
 
-.aside {
+.sidebar {
     width: 300px;
-    background-color: aqua;
+    border: 1px solid rgba(69, 199, 255, 0.096);
+    
+
 }
 
 .main {
-    background-color: blueviolet;
+    /* background-color: blueviolet; */
     flex: 1;
+}
+.sidebar-header {
+  display: flex;
+  justify-content: center;
+}
+.logo{
+    max-width: 250px;
+    
 }
 </style>
