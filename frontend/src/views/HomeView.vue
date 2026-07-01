d99ca4f257c4825e60d5b7dacc6fa99d6185f24c
Hellen Bianchini <hellenbiangod@gmail.com>
2026-07-01 10:22:01 -0300

    feat(frontend): adiciona Imagem principal de fundo na Home

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,8 +1,34 @@
 <template>
-
+    <section class="imagem-background">
+        <img src="/img-fundo-home.png" alt="Imagem de fundo" />
+    </section>
 </template>
 
-<script lang="ts" setup>
-</script>
+<style scoped>
+.imagem-background {
+    position: absolute;
+    right: 0;
+    width: calc(100vw - 350px);
+    height: 380px;
+    overflow: hidden;
+    border-radius: 0 0 0 16px;
+}
 
-<style scoped></style>
+.imagem-background img {
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: right center;
+    filter: brightness(70%);
+    -webkit-mask-image: linear-gradient(to right,
+            transparent 0%,
+            black 35%,
+            black 100%);
+
+    mask-image: linear-gradient(to right,
+            transparent 0%,
+            black 35%,
+            black 100%);
+}
+</style>
